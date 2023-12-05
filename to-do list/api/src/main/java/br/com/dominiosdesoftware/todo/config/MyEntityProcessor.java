d958c3b4f0279359b13b04f2b47df5609aaab21b
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-12-05 19:39:52 -0300

    feat: remove links da response para melhor visualização

---
+++ to-do list/api/src/main/java/br/com/dominiosdesoftware/todo/config/MyEntityProcessor.java
@@ -0,0 +1,16 @@
+package br.com.dominiosdesoftware.todo.config;
+
+import br.com.dominiosdesoftware.todo.entity.Todo;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
+
+@Configuration
+public class MyEntityProcessor implements RepresentationModelProcessor<EntityModel<Todo>> {
+
+    @Override
+    public EntityModel<Todo> process(EntityModel<Todo> model) {
+        return EntityModel.of(model.getContent());
+    }
+
+}
