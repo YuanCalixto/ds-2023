d89aee201a0f2c15f996e43b620933219a014763
Yuan <yuancalixto@gmail.com>
2023-12-06 14:53:58 -0300

    starting-commit
    Iniciando o Front

---
+++ to-do list/front/src/app/screens/tarefas/tarefas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarefas',
+  templateUrl: './tarefas.component.html',
+  styleUrls: ['./tarefas.component.css']
+})
+export class TarefasComponent {
+
+}
