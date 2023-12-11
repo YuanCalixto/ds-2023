4156e995ef6291817ad03a23561a4ce2d01b8f4f
Yuan <yuancalixto@gmail.com>
2023-12-11 15:21:45 -0300

    feat/tags
    Adição e remoção de tags

--- to-do list/front/src/app/screens/tarefas/detalhes-tarefa/detalhes-tarefa.dialog.ts
+++ to-do list/front/src/app/screens/tarefas/detalhes-tarefa/detalhes-tarefa.dialog.ts
@@ -10,6 +10,11 @@ import { Tasks } from 'src/app/core/domain/entity/tasks';
 export class DetalhesTarefaDialog implements OnInit {
   task: Tasks;
 
+  newTag: string = '';
+
+  tagInput = false;
+
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: { task: Tasks },
     private dialogRef: MatDialogRef<DetalhesTarefaDialog>
@@ -19,11 +24,27 @@ export class DetalhesTarefaDialog implements OnInit {
 
   ngOnInit() {}
 
-  editTask(): void {}
-
-  removeTask(): void {}
-
   saveTask(): void {
     this.dialogRef.close(this.task);
+  }
+
+  addTag(): void {
+    if (this.newTag.trim() !== '') {
+      const newTag = {
+        id: this.task.tags.length + 1,
+        name: this.newTag,
+        dateCreated: new Date(),
+      };
+      this.task.tags.push(newTag);
+      this.newTag = '';
+    }
+  }
+
+  removeTag(tagToRemove: any): void {
+    this.task.tags = this.task.tags.filter((tag) => tag.id !== tagToRemove.id);
+  }
+
+  toggleTagInput(): void {
+    this.tagInput = !this.tagInput;
   }
 }
