66188ab718bd3cdc2c7fe3d3337f5c536851d4f6
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-12-13 00:02:19 -0300

    fix: habilita alteração do estado da tarefa

--- to-do list/api/src/main/java/br/com/dominiosdesoftware/todo/dtos/TodoDto.java
+++ to-do list/api/src/main/java/br/com/dominiosdesoftware/todo/dtos/TodoDto.java
@@ -1,6 +1,8 @@
 package br.com.dominiosdesoftware.todo.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record TodoDto(@NotBlank String name, @NotBlank String description) {
+public record TodoDto(@NotBlank String name, @NotBlank String description, @NotNull Boolean completed) {
+
 }
