05db1e7466ae1d503a49c5d1d30aa76bbbc6ebd7
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-12-13 18:03:57 -0300

    feat: controller para registro e login de usuários

---
+++ to-do list/api/src/main/java/br/com/dominiosdesoftware/todo/controllers/AuthenticationController.java
@@ -0,0 +1,47 @@
+package br.com.dominiosdesoftware.todo.controllers;
+
+import br.com.dominiosdesoftware.todo.dtos.AuthenticationDTO;
+import br.com.dominiosdesoftware.todo.dtos.RegisterDTO;
+import br.com.dominiosdesoftware.todo.models.user.User;
+import br.com.dominiosdesoftware.todo.repositories.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@Valid @RequestBody AuthenticationDTO data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@Valid @RequestBody RegisterDTO data) {
+        if (this.userRepository.findByLogin(data.login()) != null) {
+            return ResponseEntity.badRequest().build();
+        }
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.login(), encryptedPassword, data.role());
+        this.userRepository.save(newUser);
+
+        return ResponseEntity.ok().build();
+    }
+}
