bbb2eb98ac9cec0f2c955f7b2a34e136a4d2a29f
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-12-13 00:11:28 -0300

    fix: algumas correções

--- to-do list/api/src/main/java/br/com/dominiosdesoftware/todo/models/TodoModel.java
+++ to-do list/api/src/main/java/br/com/dominiosdesoftware/todo/models/TodoModel.java
@@ -6,42 +6,45 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "TB_TODOS")
+@Table(name = "tb_todo")
 @Getter
 @Setter
-public class TodoModel implements Serializable {
-
-    private static final long serialVersionUID = 1L;
+public class TodoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "todo_id")
+    @Column(name = "id")
     private UUID id;
 
-    @Column(name = "name", nullable = false, length = 255)
+    @Column(name = "name")
     private String name;
 
-    @Column(name = "description", length = 1000)
+    @Column(name = "description")
     private String description;
 
     @Column(name = "completed")
     private boolean completed;
 
-    @Column(name = "date_created", updatable = false)
+    @Column(name = "date_created", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     @CreationTimestamp
     private Date dateCreated;
 
-    @Column(name = "last_updated")
+    @Column(name = "last_updated", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     @UpdateTimestamp
     private Date lastUpdated;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private UserModel user;
-}
 
+    @ManyToOne
+    @JoinColumn(name = "tag_id")
+    private TagModel tag;
+}
