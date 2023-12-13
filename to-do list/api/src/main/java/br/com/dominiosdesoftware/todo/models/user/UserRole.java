19464d850beb1f0ad20db81965167360cf4aed24
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-12-13 18:08:41 -0300

    feat: cria o perfil de admin e user para o aplicativo

---
+++ to-do list/api/src/main/java/br/com/dominiosdesoftware/todo/models/user/UserRole.java
@@ -0,0 +1,16 @@
+package br.com.dominiosdesoftware.todo.models.user;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
